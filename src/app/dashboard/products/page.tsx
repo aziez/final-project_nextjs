f8ad231555b432c515b18310279289a6cc869fa6
Aziez <Aziz13.id@gmail.com>
2024-03-06 21:49:39 +0700

    workshop 6 progress deploy

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,19 +1,28 @@
+import React from 'react'
+
 export default function Page() {
   return (
-    <div className="mt-5 flex justify-center">
-      <h1>PRODUCT</h1>
-      <div className="w-full">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In repellat
-        modi accusantium unde quod maxime obcaecati inventore. Repudiandae
-        incidunt magni libero ex fugit, autem nostrum. Eos necessitatibus libero
-        aliquid corrupti quasi quaerat quos dolores nemo? Dolores asperiores
-        porro, ipsam modi veritatis accusamus ex laboriosam illo maiores
-        inventore deserunt distinctio quas eos totam fugiat quae. Modi magni
-        quas porro qui provident doloremque adipisci, suscipit sunt sequi veniam
-        corrupti, incidunt odit neque magnam delectus minima consequatur! Nisi
-        pariatur quod eligendi et vero provident deserunt illum enim labore
-        maxime, delectus dolores repudiandae reprehenderit est sequi officiis
-        tempore voluptas rerum consequatur dolore voluptates. Aperiam!
+    <div className="mt-5 flex flex-col items-center">
+      <h1 className="mb-3 text-3xl font-bold">PRODUCT</h1>
+      <div className="w-full max-w-2xl text-center">
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In repellat
+          modi accusantium unde quod maxime obcaecati inventore. Repudiandae
+          incidunt magni libero ex fugit, autem nostrum. Eos necessitatibus
+          libero aliquid corrupti quasi quaerat quos dolores nemo? Dolores
+          asperiores porro, ipsam modi veritatis accusamus ex laboriosam illo
+          maiores inventore deserunt distinctio quas eos totam fugiat quae. Modi
+          magni quas porro qui provident doloremque adipisci, suscipit sunt
+          sequi veniam corrupti, incidunt odit neque magnam delectus minima
+          consequatur!
+        </p>
+        <p className="mb-4">
+          Nisi pariatur quod eligendi et vero provident deserunt illum enim
+          labore maxime, delectus dolores repudiandae reprehenderit est sequi
+          officiis tempore voluptas rerum consequatur dolore voluptates.
+          Aperiam!
+        </p>
+        {/* Add more content or components as needed */}
       </div>
     </div>
   )
